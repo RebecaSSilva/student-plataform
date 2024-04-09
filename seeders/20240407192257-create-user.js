0b6e2ac0da0018e489994bb0ba2a52749836072d
Rebeca <rebeca99rs@gmail.com>
2024-04-09 00:21:58 -0300

    ajustando seed

--- seeders/20240407192257-create-user.js
+++ seeders/20240407192257-create-user.js
@@ -1,16 +1,27 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+const faker = require('faker');
+
 const { User } = require('../models');
 
 module.exports = {
   up: async () => {
-    await User.bulkCreate([
-      {
-        email: 'user1@example.com',
-        password: bcrypt('password1'),
-        typeUser: 1,
-      }
-    ]);
+    const users = [];
+
+    for (let i = 0; i < 10; i++) {
+      const email = faker.internet.email();
+      const password = await bcrypt.hash(faker.internet.password(), 10);
+      const typeUser = faker.random.number({ min: 1, max: 3 }); 
+
+      users.push({
+        email,
+        password,
+        typeUser
+      });
+    }
+
+    await User.bulkCreate(users);
   },
 
   down: async () => {
