6ec6b5b536269c774878532e1b9d9aa21fc56adc
Rebeca <rebeca99rs@gmail.com>
2024-04-09 22:38:04 -0300

    configurando back-end no login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginRegister/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginRegister from './components/LoginRegister.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginRegister
   }
 }
 </script>
