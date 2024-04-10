176906400510be855f22bee293657133c80bddb8
Rebeca <rebeca99rs@gmail.com>
2024-04-09 22:42:18 -0300

    adicionando db novamente

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  multipleStatements: true
+});
+
+module.exports = {
+  getConnection: function() {
+    return pool;
+  }
+};
