98030fe8472ae2aa184e081a3fb7b31548b68011
Rebeca <rebeca99rs@gmail.com>
2024-04-14 15:51:04 -0300

    ajustando arquivos docker

--- backend/createDatabase.js
+++ backend/createDatabase.js
@@ -1,19 +1,24 @@
-const { Sequelize } = require('sequelize');
+const  Sequelize  = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-// Load the database configuration file
+// Carregar o arquivo de configuração do banco de dados
 const configPath = path.resolve(__dirname, 'config', 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-// Set up the database connection
-const sequelize = new Sequelize(config[process.env.NODE_ENV]);
+// Configurar a conexão com o banco de dados
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT || config[process.env.NODE_ENV].dialect, 
+  host: process.env.DB_HOST || config[process.env.NODE_ENV].host,
+  port: process.env.DB_PORT || config[process.env.NODE_ENV].port,
+  username: process.env.DB_USER || config[process.env.NODE_ENV].username,
+  password: process.env.DB_PASS || config[process.env.NODE_ENV].password,
+  database: process.env.DB_NAME || config[process.env.NODE_ENV].database
+});
 
-// Get the database name from the configuration file
-const databaseName = config[process.env.NODE_ENV].database;
-
-// Function to create the database
+// Função para criar o banco de dados
 async function createDatabase() {
+  const databaseName = sequelize.config.database;
   try {
     await sequelize.query(`CREATE DATABASE IF NOT EXISTS ${databaseName};`);
     console.log(`Database '${databaseName}' created successfully`);
