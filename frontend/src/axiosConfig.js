8457b9a0e22066e34da3265be2dfc2c0ae1021fb
Rebeca <rebeca99rs@gmail.com>
2024-04-14 20:33:14 -0300

    ajustes no login

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/student', // Define a base URL para todas as solicitações
+  baseURL: 'http://localhost:8000/api/student', // Set base URL
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8081', // Define a origem permitida para CORS
+    'Access-Control-Allow-Origin': 'http://localhost:8081', // Set an allowed origin for CORS
     'Content-Type': 'application/json'
 
   },
 });
 
-// Interceptar solicitações para adicionar o token de autenticação, se disponível
+// Intercept requests to add the authentication token if available
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
