c8e5ce2b5d30a5842756ba1055cadd577642caef
Rebeca <rebeca99rs@gmail.com>
2024-04-14 21:39:04 -0300

    adicionando css

--- backend/__tests__/studentController.test.js
+++ backend/__tests__/studentController.test.js
@@ -5,152 +5,152 @@ jest.mock('../services/studentService');
 // Importe o studentController e o studentService
 
 describe('getStudent', () => {
-  afterEach(() => {
-    jest.clearAllMocks(); 
-  });
+afterEach(() => {
+  jest.clearAllMocks(); 
+});
 
-  it('should return students', async () => {
-    const mockStudents = [{ id: 1, name: 'John', email: 'john@hotmail.com', cpf: '40825418020', ra: '12345678' }];
-    const req = {};
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should return students', async () => {
+  const mockStudents = [{ id: 1, name: 'John', email: 'john@hotmail.com', cpf: '40825418020', ra: '12345678' }];
+  const req = {};
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    // Mockando a função getStudents corretamente
-    studentService.getStudents = jest.fn().mockResolvedValue(mockStudents);
+  // Mockando a função getStudents corretamente
+  studentService.getStudents = jest.fn().mockResolvedValue(mockStudents);
 
-    await studentController.getStudent(req, res, next);
+  await studentController.getStudent(req, res, next);
 
-  });
+});
 
-  it('should handle errors', async () => {
-    const errorMessage = 'Error fetching students';
-    const req = {};
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should handle errors', async () => {
+  const errorMessage = 'Error fetching students';
+  const req = {};
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    // Mockando a função getStudents para retornar um erro
-    studentService.getStudents = jest.fn().mockRejectedValue(new Error(errorMessage));
+  // Mockando a função getStudents para retornar um erro
+  studentService.getStudents = jest.fn().mockRejectedValue(new Error(errorMessage));
 
-    await studentController.getStudent(req, res, next);
+  await studentController.getStudent(req, res, next);
 
-  });
+});
 });
 
 describe('createStudent', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-  it('should create a new student', async () => {
-    const mockStudentData = { name: 'John Doe', email: 'john.doe@example.com', cpf: '40825418020' };
-    const req = { body: mockStudentData };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should create a new student', async () => {
+  const mockStudentData = { name: 'John Doe', email: 'john.doe@example.com', cpf: '40825418020' };
+  const req = { body: mockStudentData };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    const mockCreatedStudent = { id: 1, ...mockStudentData };
-    studentService.createStudent = jest.fn().mockResolvedValue(mockCreatedStudent);
+  const mockCreatedStudent = { id: 1, ...mockStudentData };
+  studentService.createStudent = jest.fn().mockResolvedValue(mockCreatedStudent);
 
-    await studentController.createStudent(req, res, next);
+  await studentController.createStudent(req, res, next);
 
-  });
+});
 
-  it('should handle errors', async () => {
-    const errorMessage = 'Error creating student';
-    const req = { body: {} };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should handle errors', async () => {
+  const errorMessage = 'Error creating student';
+  const req = { body: {} };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    studentService.createStudent = jest.fn().mockRejectedValue(new Error(errorMessage));
+  studentService.createStudent = jest.fn().mockRejectedValue(new Error(errorMessage));
 
-    await studentController.createStudent(req, res, next);
+  await studentController.createStudent(req, res, next);
 
-  });
+});
 });
 
 
 describe('updateStudent', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-  it('should update an existing student', async () => {
-    const mockStudentId = 1;
-    const mockStudentData = { name: 'John Doe', email: 'john.doe@example.com' };
-    const req = { params: { id: mockStudentId }, body: mockStudentData };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should update an existing student', async () => {
+  const mockStudentId = 1;
+  const mockStudentData = { name: 'John Doe', email: 'john.doe@example.com' };
+  const req = { params: { id: mockStudentId }, body: mockStudentData };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    const mockUpdatedStudent = { id: mockStudentId, ...mockStudentData };
-    studentService.updateStudent.mockResolvedValue(mockUpdatedStudent);
+  const mockUpdatedStudent = { id: mockStudentId, ...mockStudentData };
+  studentService.updateStudent.mockResolvedValue(mockUpdatedStudent);
 
-    await studentController.updateStudent(req, res, next);
+  await studentController.updateStudent(req, res, next);
 
-  });
+});
 
-  it('should handle errors', async () => {
-    const errorMessage = 'Error updating student';
-    const req = { params: { id: 1 }, body: { name: 'John Doe', email: 'john.doe@example.com' } };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should handle errors', async () => {
+  const errorMessage = 'Error updating student';
+  const req = { params: { id: 1 }, body: { name: 'John Doe', email: 'john.doe@example.com' } };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    studentService.updateStudent.mockRejectedValue(new Error(errorMessage));
+  studentService.updateStudent.mockRejectedValue(new Error(errorMessage));
 
-    await studentController.updateStudent(req, res, next);
+  await studentController.updateStudent(req, res, next);
 
-  });
+});
 });
 
 describe('deleteStudent', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-  it('should delete an existing student', async () => {
-    const mockStudentId = 1;
-    const req = { params: { id: mockStudentId } };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should delete an existing student', async () => {
+  const mockStudentId = 1;
+  const req = { params: { id: mockStudentId } };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    const mockDeleteResult = { message: 'Student deleted successfully' };
-    studentService.deleteStudent.mockResolvedValue(mockDeleteResult);
+  const mockDeleteResult = { message: 'Student deleted successfully' };
+  studentService.deleteStudent.mockResolvedValue(mockDeleteResult);
 
-    await studentController.deleteStudent(req, res, next);
+  await studentController.deleteStudent(req, res, next);
 
-  });
+});
 
-  it('should handle errors', async () => {
-    const errorMessage = 'Error deleting student';
-    const req = { params: { id: 1 } };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
+it('should handle errors', async () => {
+  const errorMessage = 'Error deleting student';
+  const req = { params: { id: 1 } };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
 
-    studentService.deleteStudent.mockRejectedValue(new Error(errorMessage));
+  studentService.deleteStudent.mockRejectedValue(new Error(errorMessage));
 
-    await studentController.deleteStudent(req, res, next);
+  await studentController.deleteStudent(req, res, next);
 
-  });
+});
 });
--- frontend/src/components/StudentView.vue
+++ frontend/src/components/StudentView.vue
@@ -1,176 +1,176 @@
 <template>
-  <div>
-    <v-data-table :headers="headers" :items="students" :search="search">
-      <template v-slot:top>
-        <div class="actions-table-section">
-          <div class="search">
-            <v-text-field
-              v-model="search"
-              label="Digite sua busca"
-              class="search-field"
-              append-inner-icon="mdi-magnify"
-              variant="outlined"
-              full-width="true"
-              hide-details
-              single-line
-            ></v-text-field>
-          </div>
-          <div class="action-add-student">
-            <v-btn class="mb-2" color="black" dark @click="$emit('openDialogStudent')">
-              Cadastrar Aluno
-            </v-btn>
-          </div>
+<div>
+  <v-data-table :headers="headers" :items="students" :search="search">
+    <template v-slot:top>
+      <div class="actions-table-section">
+        <div class="search">
+          <v-text-field
+            v-model="search"
+            label="Digite sua busca"
+            class="search-field"
+            append-inner-icon="mdi-magnify"
+            variant="outlined"
+            full-width="true"
+            hide-details
+            single-line
+          ></v-text-field>
         </div>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Tem certeza que deseja deletar esse estudante?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
-                >Cancelar</v-btn
-              >
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="deleteItemConfirm"
-                >Deletar</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </template>
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="$emit('editStudent', item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-      </template>
-    </v-data-table>
-      <div class="pagination-controls">
-        <v-btn @click="prevPage" :disabled="currentPage === 1">Anterior</v-btn>
-        <span class="page">Página {{ currentPage }}</span>
-        <v-btn @click="nextPage" :disabled="currentPage * pageSize >= totalStudentsCount">Próximo</v-btn>
+        <div class="action-add-student">
+          <v-btn class="mb-2" color="black" dark @click="$emit('openDialogStudent')">
+            Cadastrar Aluno
+          </v-btn>
+        </div>
       </div>
-  </div>
+      <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-card>
+          <v-card-title class="text-h5"
+            >Tem certeza que deseja deletar esse estudante?</v-card-title
+          >
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
+              >Cancelar</v-btn
+            >
+            <v-btn
+              color="blue-darken-1"
+              variant="text"
+              @click="deleteItemConfirm"
+              >Deletar</v-btn
+            >
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-icon small class="mr-2" @click="$emit('editStudent', item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+  </v-data-table>
+    <div class="pagination-controls">
+      <v-btn @click="prevPage" :disabled="currentPage === 1">Anterior</v-btn>
+      <span class="page">Página {{ currentPage }}</span>
+      <v-btn @click="nextPage" :disabled="currentPage * pageSize >= totalStudentsCount">Próximo</v-btn>
+    </div>
+</div>
 </template>
 
 <script>
 import axios from '@/axiosConfig';
 
 export default {
-  name: "StudentView",
- 
-  mounted(){
+name: "StudentView",
+
+mounted(){
+  this.getStudents();
+},
+data: () => ({
+  search: "",
+  dialogDelete: false,
+  currentPage: 1, 
+  pageSize: 10, 
+  headers: [
+    {
+      title: "RA",
+      align: "start",
+      sortable: true,
+      key: "ra",
+    },
+    { title: "Nome", key: "name", sortable: true },
+    { title: "E-mail", key: "email", sortable: true },
+    { title: "CPF", key: "cpf", sortable: true },
+    { title: "Ações", key: "actions", sortable: false },
+  ],
+  students: [],
+  editedItem: {
+    registroAcademicio: "",
+    nome: "",
+    cpf: "",
+  },
+  defaultItem: {
+    registroAcademicio: "",
+    nome: "",
+    cpf: "",
+  },
+}),
+watch: {
+  updateList() {
     this.getStudents();
   },
-  data: () => ({
-    search: "",
-    dialogDelete: false,
-    currentPage: 1, 
-    pageSize: 10, 
-    headers: [
-      {
-        title: "RA",
-        align: "start",
-        sortable: true,
-        key: "ra",
-      },
-      { title: "Nome", key: "name", sortable: true },
-      { title: "E-mail", key: "email", sortable: true },
-      { title: "CPF", key: "cpf", sortable: true },
-      { title: "Ações", key: "actions", sortable: false },
-    ],
-    students: [],
-    editedItem: {
-      registroAcademicio: "",
-      nome: "",
-      cpf: "",
-    },
-    defaultItem: {
-      registroAcademicio: "",
-      nome: "",
-      cpf: "",
-    },
-  }),
-  watch: {
-    updateList() {
-      this.getStudents();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
+  dialogDelete(val) {
+    val || this.closeDelete();
   },
-  methods: {
-    async getStudents() {
-      try {
-        const response = await axios.get('/', {
-          params: {
-            page: this.currentPage,
-            pageSize: this.pageSize
-          }
-        });
-        this.students = response.data;
-      } catch (error) {
-        this.$emit("snackbar", { show: true, text: "The server was unable to respond. Please try again later.", color: "error" })
-        this.$emit("updateList");
-      }
-    },
-    async goToPage(page) {
-      this.currentPage = page;
-      this.getStudents();
-    },
-    async nextPage() {
-      this.currentPage++;
-      this.getStudents();
-    },
-    async prevPage() {
-      if (this.currentPage > 1) {
-        this.currentPage--;
-        this.getStudents();
-      }
-    },
-    deleteItem(item) {
-      this.editedIndex = this.students.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    async deleteItemConfirm() {
-      try {
-        const response = await axios.delete(`/${this.editedItem.id}`);
-        if (response.status === 200) {
-          this.$emit("snackbar", {show: true, text:`Student ${this.editedItem.name} was deleted succesfuly.`, color:"success"})
-          this.closeDelete();
-          this.getStudents();
-        } else {
-          this.$emit("snackbar", {show: true, text:`Failed to delet student.`, color:"error"})
+},
+methods: {
+  async getStudents() {
+    try {
+      const response = await axios.get('/', {
+        params: {
+          page: this.currentPage,
+          pageSize: this.pageSize
         }
-      } catch (error) {
-        this.$emit("snackbar", {show: true, text:"Server was unable to respond.Please try again later", color:"error"})
-      }
-    },
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
       });
-    },
+      this.students = response.data;
+    } catch (error) {
+      this.$emit("snackbar", { show: true, text: "The server was unable to respond. Please try again later.", color: "error" })
+      this.$emit("updateList");
+    }
   },
+  async goToPage(page) {
+    this.currentPage = page;
+    this.getStudents();
+  },
+  async nextPage() {
+    this.currentPage++;
+    this.getStudents();
+  },
+  async prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.getStudents();
+    }
+  },
+  deleteItem(item) {
+    this.editedIndex = this.students.indexOf(item);
+    this.editedItem = Object.assign({}, item);
+    this.dialogDelete = true;
+  },
+
+  async deleteItemConfirm() {
+    try {
+      const response = await axios.delete(`/${this.editedItem.id}`);
+      if (response.status === 200) {
+        this.$emit("snackbar", {show: true, text:`Student ${this.editedItem.name} was deleted succesfuly.`, color:"success"})
+        this.closeDelete();
+        this.getStudents();
+      } else {
+        this.$emit("snackbar", {show: true, text:`Failed to delet student.`, color:"error"})
+      }
+    } catch (error) {
+      this.$emit("snackbar", {show: true, text:"Server was unable to respond.Please try again later", color:"error"})
+    }
+  },
+  closeDelete() {
+    this.dialogDelete = false;
+    this.$nextTick(() => {
+      this.editedItem = Object.assign({}, this.defaultItem);
+      this.editedIndex = -1;
+    });
+  },
+},
 };
 </script>
 
 <style>
 .v-data-table-footer{
-  display: none;
+display: none;
 }
 .pagination-controls {
-  display: flex;
-  justify-content: flex-end; 
-  margin: 10px;
+display: flex;
+justify-content: flex-end; 
+margin: 10px;
 }
 .page{
-  margin:10px
+margin:10px
 }
 </style>
